6b5519189c1da3c8ccd9636a405204790343d126
Fredrick Boaz <fredrickboaz@gmail.com>
2022-09-18 09:52:25 +0300

    room service

--- vendor/filament/notifications/stubs/scaffolding/resources/js/app.js
+++
@@ -1,8 +0,0 @@
-import Alpine from 'alpinejs'
-import NotificationsAlpinePlugin from '../../vendor/filament/notifications/dist/module.esm'
-
-Alpine.plugin(NotificationsAlpinePlugin)
-
-window.Alpine = Alpine
-
-Alpine.start()
--- vendor/filament/tables/resources/views/actions/icon-button-action.blade.php
+++
@@ -1,6 +0,0 @@
-<x-tables::actions.action
-    :action="$action"
-    :label="$getLabel()"
-    component="tables::icon-button"
-    class="-my-2 filament-tables-icon-button-action"
-/>

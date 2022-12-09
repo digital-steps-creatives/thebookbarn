b0ca81d7df458d0b6bc2ed3ce8eccf918c1a285a
Fredrick Boaz <fredrickboaz@gmail.com>
2022-12-09 17:35:28 +0300

    stuff

--- resources/js/Pages/Affiliates/Dashboard.vue
+++ resources/js/Pages/Affiliates/Dashboard.vue
@@ -12,20 +12,75 @@ import Welcome from '@/Components/Welcome.vue';
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-4">
                 <div class="mb-5">
-                    <h2>Affiliate Program</h2>
-                    <p>Share your referral code to potential customers and earn 10% of their purchase when they make payment.</p>
+                    <div class="font-bold text-xl text-gray-800 leading-none mb-4">Good day, {{ $page.props.user.name }}</div>
+                    <h2 class="font-normal text-2xl text-primary">Affiliate Program</h2>
+                    <p>Share your referral code to potential customers and earn a commission.</p>
                 </div>
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                   <div class="p-4 flex">
-                        <div class="justify-start">
-                            <h4>Affiliate Stats</h4>
+                <div class="overflow-hidden shadow-xl sm:rounded-lg mb-4">
+                   <div class="bg-green-600  px-5 py-2 flex justify-between">
+                        <div>
+                            <h4 class="text-white">Affiliate Stats</h4>
                         </div>
-                        <div class="justify-end">
-                            (Updates after every 30 minutes)
+                        <div>
+                            <span class="text-white">(Updates after every 30 minutes)</span>
                         </div>
                    </div>
+                   
+                    <div class="px-5 py-4 pb-5 bg-white">
+                            <div class="grid grid-cols-4 gap-4">
+                                <div class="p-4 bg-green-100 rounded-xl text-gray-800">
+                                <div class="font-bold text-2xl leading-none">20</div>
+                                <div class="mt-2">Referrals</div>
+                                </div>
+                                <div class="p-4 bg-green-100 rounded-xl text-gray-800">
+                                <div class="font-bold text-2xl leading-none">5,5</div>
+                                <div class="mt-2">Balance</div>
+                                </div>
+                                <div class="p-4 bg-green-100 rounded-xl text-gray-800">
+                                <div class="font-bold text-2xl leading-none">20</div>
+                                <div class="mt-2">Total Earnings</div>
+                                </div>
+                                <div class="p-4 bg-green-100 rounded-xl text-gray-800">
+                                <div class="font-bold text-2xl leading-none">5,5</div>
+                                <div class="mt-2">Paid Commissions</div>
+                                </div>
+                            </div>
+            
+          </div>
+                </div>
+                <div class="overflow-hidden shadow-xl sm:rounded-lg">
+                   <div class="bg-green-600  px-5 py-2 flex justify-between">
+                        <div>
+                            <h4 class="text-white">Update your Details</h4>
+                        </div>
+                        <div>
+                            
+                        </div>
+                   </div>
+                   
+                    <div class="px-5 py-4 pb-5 bg-white">
+                            <div class="grid grid-cols-4 gap-4">
+                                <div class="p-4 bg-green-100 rounded-xl text-gray-800">
+                                <div class="font-bold text-2xl leading-none">20</div>
+                                <div class="mt-2">Referrals</div>
+                                </div>
+                                <div class="p-4 bg-green-100 rounded-xl text-gray-800">
+                                <div class="font-bold text-2xl leading-none">5,5</div>
+                                <div class="mt-2">Balance</div>
+                                </div>
+                                <div class="p-4 bg-green-100 rounded-xl text-gray-800">
+                                <div class="font-bold text-2xl leading-none">20</div>
+                                <div class="mt-2">Total Earnings</div>
+                                </div>
+                                <div class="p-4 bg-green-100 rounded-xl text-gray-800">
+                                <div class="font-bold text-2xl leading-none">5,5</div>
+                                <div class="mt-2">Paid Commissions</div>
+                                </div>
+                            </div>
+            
+          </div>
                 </div>
             </div>
         </div>

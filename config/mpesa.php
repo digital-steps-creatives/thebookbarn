5c6fa4d21b86afad208a2a8cdbba1742f39c0f3f
Fredrick Boaz <fredrickboaz@gmail.com>
2023-01-10 09:05:39 +0300

    mpesa integration

--- config/mpesa.php
+++ config/mpesa.php
@@ -9,11 +9,11 @@ return [
     | This is the default account to be used when none is specified.
     */
 
-    'paybill' => env('MPESA_PAYBILL_NUMBER', '600988'),
-    'shortcode' => env('MPESA_PAYBILL_NUMBER', '600988'),
-    'passkey' => env('MPESA_PASS_KEY', ''),
-    'type' => env('MPESA_ENV', 'sandbox'),
-    'consumer_key' => env('MPESA_CONSUMER_KEY', 'OSGHjreXoAoBnlJsfYFA7lRA4soUaVPz'),
-    'consumer_secret' => env('MPESA_CONSUMER_SECRET', 'kmriYXlBh81p3l6J'),
+    'paybill' => env('MPESA_PAYBILL_NUMBER', '4102381'),
+    'shortcode' => env('MPESA_PAYBILL_NUMBER', '4102381'),
+    'passkey' => env('MPESA_PASS_KEY', '796bd1d24c267779779bca494b51bd924d513ad205d658334576053cb2737913'),
+    'type' => env('MPESA_ENV', 'live'),
+    'consumer_key' => env('MPESA_CONSUMER_KEY', '8AguWBcPeDx0y20IZFjvDGfaAX1vDvTK'),
+    'consumer_secret' => env('MPESA_CONSUMER_SECRET', 'vMh3HsCN4jUeMEDO'),
     'url' => env('MPESA_URL', '')
 ];

8db4b46774096acf035c79e3755de85b294d80ad
Fredrick Boaz <fredrickboaz@gmail.com>
2023-02-07 14:50:51 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,17 @@ export default defineConfig({
             },
         })
     ],
+    build: {
+        rollupOptions: {
+            output:{
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    },
     resolve: {
         alias: {
           '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
